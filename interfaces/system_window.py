b319a861021a9d1262a0092343b36e682d1c87f0
Danilosv337 <danilo_santos.vieira@hotmail.com>
2023-04-12 22:03:22 -0300

    repair bug

--- interfaces/system_window.py
+++ interfaces/system_window.py
@@ -3,6 +3,7 @@ from PySide6.QtWidgets import QFileDialog
 from PySide6.QtGui import QIcon
 from modules.system import teste_walk
 import eyed3
+from threading import Thread
 
 class App(QWidget,Ui_Form):
     def __init__(self,parent= None) -> None:
@@ -15,35 +16,40 @@ class App(QWidget,Ui_Form):
         self.btn_select.clicked.connect(self.get_directory)
         self.btn_organizer.clicked.connect(lambda: self.organize(self.way))
         self.mp3_load.setValue(0)
+        self.td1 = Thread()
+        self.td2 = Thread()
+        self.td3 = Thread()
 
 
     def get_directory(self):
         self.way = QFileDialog.getExistingDirectory()
         self.inputtext_way.setText(f"{self.way}")
     def organize(self,way):
-        try:
-            files = teste_walk(way)
-            if files == []: 
-                self.edittext_message.setStyleSheet("font-size: 20px; color: red;")
-                self.edittext_message.setText('Sem Arquivos ".mp3"')
-                return
-            self.mp3_load.setMaximum(len(files))
-            num: int = 0
-            for way_file,file in files:
+
+        files = teste_walk(way)
+        if files == []: 
+            self.edittext_message.setStyleSheet("font-size: 20px; color: red;")
+            self.edittext_message.setText('Sem Arquivos ".mp3"')
+            return
+        self.mp3_load.setMaximum(len(files))
+        num: int = 0
+        for way_file,file in files:
+            try:
                 num += 1
                 self.mp3_load.setValue(num)
                 audio = eyed3.load(way_file)
-                if not audio.tag:
+                if not audio.tag or audio.tag == None:
                     audio.initTag()
                 audio.tag.title = file.replace(".mp3","")
                 audio.tag.save(version=(2,3,0))
-            self.edittext_message.setStyleSheet("font-size: 20px; color: green;")
-            self.edittext_message.setText("Finalizado!")
-        except Exception as erro:
-            with open("mp3organizer.log","a") as logfile:
-                logfile.write(erro)
-            self.edittext_message.setStyleSheet("font-size: 20px; color: red;")
-            self.edittext_message.setText('Ocorreu um erro')
+                self.edittext_message.setStyleSheet("font-size: 20px; color: green;")
+                self.edittext_message.setText(f"Música {file} Finalizada!")
+            except Exception as erro:
+                with open("mp3organizer.log","a") as logfile:
+                    logfile.write(f"O arquivo {file} Apresentou o erro: \n \
+                                  {erro} \n\n")
+                    self.edittext_message.setStyleSheet("font-size: 20px; color: red;")
+                    self.edittext_message.setText(f'A erro na música: {file}')
 
 
             
